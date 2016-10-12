c7ec8ffa91b00b0d3ffbcec457d9f8095997b266
tavindersingh <tavinder123singh@gmail.com>
2016-10-12 05:27:36 +0000

    First Commit

---
+++ mysite/static/base.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+        $("div>p>b").animate({
+            left: '0px'
+
+        },1500);
+
+        $("div>p>span").animate({
+            left: '0px'
+        },1500);
+});
